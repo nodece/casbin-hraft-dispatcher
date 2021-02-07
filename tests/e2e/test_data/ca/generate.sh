68a9a36a6913a4531bd7336ff4c16f3448096203
Zixuan Liu <nodeces@gmail.com>
2021-02-07 22:35:56 +0800

    refactor: rewrite all
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- tests/e2e/test_data/ca/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cfssl gencert -initca ca-csr.json | cfssljson -bare ca
-cfssl gencert -ca=./ca.pem -ca-key=./ca-key.pem  -config=./ca-config.json -profile=peer peer-csr.json | cfssljson -bare peer
